0dcffb17748dd551ef759725be1c90534bd3cac9
ProfessoraStefanyGalindo <galindostefany7@gmail.com>
2025-07-05 01:06:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+// No momento, funcionalidades básicas (como o formulário de contato e alertas de compra)
+// estão diretamente nos HTMLs das respectivas páginas para simplicidade.
+// Este arquivo pode ser usado para adicionar funcionalidades globais futuras.
